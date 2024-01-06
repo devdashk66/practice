e0ccc71064d657c70c300984b2d23113953f6207
devdashk66 <devdashk66@gmail.com>
2024-01-06 17:12:00 +0600

    country name added

---
+++ app/components/Country.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Country = () => {
+  return <div className="text-4xl mt-6">My counrty name in Bangladesh</div>;
+};
+
+export default Country;
--- app/page.js
+++ app/page.js
@@ -1,5 +1,6 @@
 "use client";
 import { useState } from "react";
+import Country from "./components/Country";
 
 export default function Home() {
   const [name, setName] = useState(false);
@@ -14,6 +15,10 @@ export default function Home() {
         <button className="px-2 py-2 bg-slate-600 mt-6" onClick={showName}>
           {!name ? "Click to show" : "Click to hide"}
         </button>
+      </main>
+
+      <main>
+        <Country></Country>
       </main>
     </>
   );
